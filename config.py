1e9f6bdadbeeea62eee2fd4fccbb9fac035b745d
Palak Taneja <tanejapalak16@gmail.com>
2026-01-05 22:11:52 +0530

    Add database config and JobApplication model using SQLAlchemy

---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI= "sqlite:///ats_lite.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ models/job.py
@@ -0,0 +1,15 @@
+from extensions import db
+from datetime import datetime
+
+class JobApplication(db.Model):
+    __tablename__ = 'job_applications'
+
+    id= db.Column(db.Integer, primary_key=True)
+    company= db.Column(db.String(100), nullable=False)
+    role= db.Column(db.String(100), nullable=False)
+    status= db.Column(db.String(50), default="Applied")
+    applied_date= db.Column(db.DateTime, default=datetime.utcnow)
+    notes= db.Column(db.Text)
+
+    def __repr__(self):
+        return f"<JobApplication {self.id}: {self.company} - {self.role}>"
